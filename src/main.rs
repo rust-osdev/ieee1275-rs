2c1b69c3b1cbb85864a98af4359e6aed6152b2e5
Alberto Ruiz <aruiz@redhat.com>
2022-04-28 22:47:08 +0200

    Attempt to get a property

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,33 @@ fn panic(_panic: &PanicInfo<'_>) -> ! {
 
 #[repr(C)]
 struct ServiceArgs {
-    service: *const char,
+    service: *const u8,
     nargs: i32,
-    ret: i32,
+    nret: i32,
 }
 
 #[no_mangle]
 #[link_section=".text"]
-extern "C" fn _start(_r3: u32, _r4: u32, _r5: extern "C" fn (*mut ServiceArgs) -> isize) -> isize {
+extern "C" fn _start(_r3: u32, _r4: u32, entry: extern "C" fn (*mut ServiceArgs) -> isize) -> isize {
+
+    #[repr(C)]
+    struct GetPropArgs {
+        args: ServiceArgs,
+        argv: [isize;4],
+        retv: [isize;1]
+    }
+
+    let mut get_prop_args = GetPropArgs {
+        args: ServiceArgs {
+            service: "getprop".as_ptr(),
+            nargs: 4,
+            nret: 1
+        },
+        argv: [0,0,0,0],
+        retv: [0]
+    };
+
+    let _ = entry (&mut get_prop_args.args);
 
     loop {}
 }
