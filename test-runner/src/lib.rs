194d85957296a918a1b3f6756b32452c7bd89464
Alberto Ruiz <aruiz@redhat.com>
2022-05-06 23:49:20 +0200

    Implement claim test

--- test-runner/src/lib.rs
+++ test-runner/src/lib.rs
@@ -1,4 +1,3 @@
-
 // Copyright 2021 Alberto Ruiz <aruiz@redhat.com>
 //
 // Use of this source code is governed by an MIT-style
@@ -9,9 +8,18 @@ extern crate ieee1275_rs;
 
 #[cfg(test)]
 mod tests {
-    use std::mem::size_of;
+    use std::{
+        collections::HashMap,
+        hash::Hash,
+        mem::{self, size_of},
+        usize,
+    };
 
-    use ieee1275_rs::{PHandle, PROM, services::{Args, FindDeviceArgs}, services};
+    use ieee1275_rs::{
+        services,
+        services::{Args, FindDeviceArgs},
+        PHandle, PROM,
+    };
 
     // Infrastructure to mock an Open Firmware implementation
 
@@ -27,9 +35,18 @@ mod tests {
         chosen_phandle: usize,
     }
 
-    static mut MOCK: MockProm = MockProm { stdout: String::new(), stdout_ihandle: STDOUT_IHANDLE, chosen_phandle: CHOSEN_PHANDLE };
+    struct Heap {
+        heap: Option<HashMap<*mut u8, Vec<u8>>>,
+    }
 
-    fn cast_args<T> (args: *mut Args) -> &'static mut T {
+    static mut MOCK: MockProm = MockProm {
+        stdout: String::new(),
+        stdout_ihandle: STDOUT_IHANDLE,
+        chosen_phandle: CHOSEN_PHANDLE,
+    };
+    static mut HEAP: Heap = Heap { heap: None };
+
+    fn cast_args<T>(args: *mut Args) -> &'static mut T {
         unsafe { &mut *(args as *mut T) }
     }
 
@@ -38,7 +55,7 @@ mod tests {
             self.stdout.clear();
         }
 
-        fn finddevice (&self, args: *mut Args) -> usize {
+        fn finddevice(&self, args: *mut Args) -> usize {
             let args = cast_args::<services::FindDeviceArgs>(args);
             let device = unsafe { std::slice::from_raw_parts(args.device, MAX_DEVICE_LENGTH) };
 
@@ -53,7 +70,7 @@ mod tests {
             }
         }
 
-        fn getprop (&self, args: *mut Args) -> usize {
+        fn getprop(&self, args: *mut Args) -> usize {
             let args = cast_args::<services::PropArgs<u8>>(args);
             let prop = unsafe { std::slice::from_raw_parts(args.prop, MAX_SERVICE_LENGTH) };
 
@@ -67,73 +84,100 @@ mod tests {
                 args.size = size_of::<usize>();
                 args.size
             } else {
+                args.size = usize::MAX;
                 usize::MAX
             }
         }
 
-        fn write (&self, args: *mut Args) -> usize {
+        fn write(&self, args: *mut Args) -> usize {
             let args = cast_args::<services::WriteArgs>(args);
-            let mock_ref = unsafe {&mut MOCK};
+            let mock_ref = unsafe { &mut MOCK };
 
             assert_eq!(args.args.nargs, 3);
             assert_eq!(args.args.nret, 1);
+
             if format!("{:p}", args.stdout) == "0xdecafbad" {
                 let msg: &[u8] = unsafe { std::slice::from_raw_parts(args.msg, args.len) };
                 let mut c: usize = 0;
                 for i in msg {
                     mock_ref.stdout.push(*i as char);
                     c += 1;
-                };
+                }
                 c
             } else {
                 usize::MAX
             }
         }
 
-        fn claim (&self, args: *mut Args) -> usize {
+        fn claim(&self, args: *mut Args) -> usize {
             let args = cast_args::<services::ClaimArgs>(args);
-            let mock_ref = unsafe {&mut MOCK};
-            0
+            let heap_ref = unsafe { &mut HEAP };
+
+            if heap_ref.heap.is_none() {
+                heap_ref.heap = Some(HashMap::new());
+            }
+
+            match &mut heap_ref.heap {
+                Some(heap) => {
+                    let mut array = vec![0 as u8; args.size];
+                    args.ret = array.as_mut_ptr();
+                    heap.insert(args.ret, array);
+                    unsafe { std::mem::transmute(args.ret) }
+                }
+                _ => {
+                    args.ret = unsafe { std::mem::transmute(usize::MAX) };
+                    usize::MAX
+                }
+            }
         }
 
-        fn release (&self, args: *mut Args) -> usize {
+        fn release(&self, args: *mut Args) -> usize {
             let args = cast_args::<services::ReleaseArgs>(args);
-            let mock_ref = unsafe {&mut MOCK};
+            let mock_ref = unsafe { &mut MOCK };
+            let heap_ref = unsafe { &mut HEAP };
+
+            if heap_ref.heap.is_none() {
+                return usize::MAX;
+            }
+
             0
         }
 
-        fn open (&self, args: *mut Args) -> usize {
+        fn open(&self, args: *mut Args) -> usize {
             let args = cast_args::<services::OpenArgs>(args);
-            let mock_ref = unsafe {&mut MOCK};
+            let mock_ref = unsafe { &mut MOCK };
             0
         }
 
-        fn read (&self, args: *mut Args) -> usize {
+        fn read(&self, args: *mut Args) -> usize {
             let args = cast_args::<services::ReadArgs>(args);
-            let mock_ref = unsafe {&mut MOCK};
+            let mock_ref = unsafe { &mut MOCK };
             0
         }
 
-        fn close (&self, args: *mut Args) -> usize {
-            let mock_ref = unsafe {&mut MOCK};
+        fn close(&self, args: *mut Args) -> usize {
+            let mock_ref = unsafe { &mut MOCK };
             0
         }
     }
 
     extern "C" fn mock_entry(args: *mut Args) -> usize {
         let service_args = unsafe { &mut (*args) };
-        let service = unsafe { std::slice::from_raw_parts(service_args.service, MAX_DEVICE_LENGTH) };
+        let service =
+            unsafe { std::slice::from_raw_parts(service_args.service, MAX_DEVICE_LENGTH) };
 
-        let mock_ref = unsafe {&mut MOCK};
-        
+        let mock_ref = unsafe { &mut MOCK };
+
         if service.starts_with(b"finddevice\0") {
             mock_ref.finddevice(args)
         } else if service.starts_with(b"getprop\0") {
             mock_ref.getprop(args)
         } else if service.starts_with(b"write\0") {
             mock_ref.write(args)
+        } else if service.starts_with(b"claim\0") {
+            mock_ref.claim(args)
         } else {
-          usize::MAX
+            usize::MAX
         }
     }
 
@@ -150,9 +194,36 @@ mod tests {
 
     #[test]
     fn write_stdout() {
-        let mock_ref = unsafe {&mut MOCK};
+        let mock_ref = unsafe { &mut MOCK };
         let prom = PROM::new(mock_entry).unwrap();
         prom.write_line("one two three");
         assert_eq!(mock_ref.stdout, "one two three\n\r");
+    }
+
+    #[test]
+    fn claim_release() {
+        let prom = PROM::new(mock_entry).unwrap();
+        let heap = unsafe { &mut HEAP };
+
+        let ret = prom.claim(4, 1);
+        assert!(ret.is_ok());
+        let buffer = ret.unwrap();
+
+        // When we call prom.claim() the Heap gets created if it doesn't exists so this is always safe
+        let heap = heap.heap.as_ref().unwrap();
+        let memchunk = heap.get(&buffer);
+        assert!(memchunk.is_some(), "Heap did not find returned address");
+        let memchunk = memchunk.unwrap();
+
+        let buffer = unsafe { std::slice::from_raw_parts_mut(buffer, 4) };
+        buffer[0] = 1 as u8;
+        buffer[1] = 2 as u8;
+        buffer[2] = 3 as u8;
+        buffer[3] = 4 as u8;
+
+        assert_eq!(
+            memchunk as &[u8], buffer,
+            "Allocated memory did not point to the same area"
+        );
     }
 }
