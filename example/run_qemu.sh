e4e9eeb0b2fc131ce545470ee9c36c0bf6aa3a26
Alberto Ruiz <aruiz@redhat.com>
2024-07-27 01:31:51 +0100

    example: simplify example image

--- example/run_qemu.sh
+++ example/run_qemu.sh
@@ -2,18 +2,17 @@
 set -e
 
 if [ ! -f disk.img ]; then
-fallocate -l 2G disk.img
+fallocate -l 200M disk.img
 sfdisk disk.img <<EOF
 label: gpt
-label-id: 4623E93C-7CA2-3643-9400-6559B3C9F7C6
+label-id: F14A008C-0832-40CC-AB26-B5C4B65BD9D7
 device: disk.img
 unit: sectors
 first-lba: 2048
-last-lba: 4194270
+last-lba: 409566
 sector-size: 512
 
-disk.img1 : start=        2048, size=       10240, type=00000000-0000-0000-0000-000000000000, uuid=5E949E3F-BEC6-A84A-BFC0-E67A2D7ACA5F
-disk.img2 : start=       12288, size=     4179968, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, uuid=98371EB3-FB56-0848-A04B-8DF3809ABED0
+disk.img1 : start=        2048, size=      405504, type=9E1A2D38-C612-4316-AA26-8B49521E5A8B, uuid=C8314A8B-6B60-4ACC-AA68-C743E6076635
 EOF
 
 fallocate -l 1.5G tmp.img
