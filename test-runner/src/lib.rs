0916a580013336de749264fd550f689b87da725e
Alberto Ruiz <aruiz@redhat.com>
2026-03-13 12:28:37 +0000

    LICENSE: Update license

--- test-runner/src/lib.rs
+++ test-runner/src/lib.rs
@@ -1,8 +1,8 @@
 // Copyright 2021 Alberto Ruiz <aruiz@redhat.com>
 //
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 extern crate ieee1275;
 
